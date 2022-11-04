6eee303adb50bae74a0128f1520f873868dcc130
saifmohamedsv <xcyberghostx1@gmail.com>
2022-11-04 21:36:48 +0200

    loader

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { HeartIcon } from "@heroicons/react/outline";
+import { StarIcon } from "@heroicons/react/solid";
 
 const InfoCard = ({
   img,
@@ -14,7 +15,7 @@ const InfoCard = ({
   lat,
 }) => {
   return (
-    <div className="flex mb-4">
+    <div className="flex mb-4 hover:shadow-lg transition duration-200 ease-out py-4 px-2 border-b cursor-pointer hover:opacity-80 rounded-md first:border-t">
       <div className="relative h-24 w-40 md:w-80 md:h-52 flex-shrink-0 ">
         <Image
           className="rounded-md"
@@ -33,7 +34,18 @@ const InfoCard = ({
 
         <div className="border-b w-10 pt-2" />
 
-        <p className="pt-2 text-sm text-gray-400">{description}</p>
+        <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+
+        <div className="flex items-end justify-between pt-5">
+          <p className="flex items-center ">
+            <StarIcon className="h-7 text-red-400" />
+            {star}
+          </p>
+          <div>
+            <p className="text-lg font-semibold pb-2 lg:text-2xl">{price}</p>
+            <p className="text-right font-extralight">{total}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import "../styles/globals.css";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
+import ProgressBar from "@badrap/bar-of-progress";
+import { Router } from "next/router";
+
+const progress = new ProgressBar({
+  size: 3,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100
+})
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
